c9e1a9419f300b836ab4ae04bc5465e96c5fd5ef
rusia9217 <110519493+unnimm@users.noreply.github.com>
2023-12-26 23:50:37 +0900

    feat: 파일 생성, Navbar 구현
    파일 생성, Navbar 구현

---
+++ airbnb-clone/app/components/navbar/Logo.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import Image from "next/image"
+import {useRouter} from "next/navigation";
+
+const Logo = () => {
+    const router = useRouter();
+    return(
+        <Image
+            alt="Logo"
+        className=""></Image>
+    )
+}
+export default Logo;
